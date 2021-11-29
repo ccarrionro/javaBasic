7298da802b7c31764ec193bced58254fe6d14baf
Carlos CR <ccarrionro@gmail.com>
2021-11-29 21:16:39 +0100

    .

---
+++ src/obc/Ejercicio6.java
@@ -0,0 +1,14 @@
+package obc;
+
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        String nombres[] = {"pepe","manolo","juan"};
+
+        for (String nombre : nombres){
+
+            System.out.println("Nombre actual "+nombre  );
+        }
+    }
+}
