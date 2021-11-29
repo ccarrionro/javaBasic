0725709ea2dd2c45943ba606be4da56421c59bfd
Carlos CR <ccarrionro@gmail.com>
2021-11-29 13:34:15 +0100

    .

--- src/obc/clases/Ejercicio2.java
+++ src/obc/clases/Ejercicio2.java
@@ -1,33 +1,40 @@
+// Programa Java curso OBC - Java Básico - Ejercicio 2
+
+/*
+  <h1>Ejercicio Sesión 2/h1>
+  Crear una función que reciba un precio y devuela el precio con el iva incluido.
+  nota: añado el envío del tipo de iva en la función, aunque se podría usar como constante o directamente.
+   *
+  @author  Carlos Carrión
+ * @version 1.0
+ * @since   29/11/2021
+ */
+
+
+
 package obc.clases;
 
 public class Ejercicio2 {
 
+        public static void main(String[] args) {
 
-        import java.util.Scanner;
+            float baseImponible = 1230.33f;
+            float importeIva;
+            int iva = 21;
 
+            importeIva = calcularIva(baseImponible,iva);
 
-    /*
-    Crear una código que te pida introducir un precio y que mediante una función
-    te devuelva el precio con IVA incluido
-     */
+            System.out.println("Base imponible: " + baseImponible + "\nIVA " + iva + "%: "+ importeIva );
+            System.out.println("Importe Total:" + (baseImponible+importeIva));
 
-    private static final double IVA_GENERAL = 1.21;
+        }
 
-    public static void main(String[] args) {
+        static float calcularIva(float baseImponible, int iva) {
+            /* calculamos el IVA dado que en méxico el iva es el 15% multiplicamos el precio por 0.15
+             *  Sumamos el precio del producto mas el iva para optener el resultado final
+             */
 
-        Scanner sc = new Scanner(System.in);
+            return baseImponible * iva / 100;
 
-        //Te pide introducir el precio
-        System.out.println("Introduzca el precio");
-        double precio = sc.nextDouble();
-
-        //Imprime el precio base y con IVA
-        System.out.println("Precio sin IVA: " + precio + "\n" + "Precio con IVA: " + getPrecioIva(precio));
-    }
-    //Calcula el precio con IVA
-    public static double getPrecioIva (double precio) {
-        return precio * IVA_GENERAL;
-
-
-    }
+        }
 }
